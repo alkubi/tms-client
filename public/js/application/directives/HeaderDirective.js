8de4c225179f86e06434e5e588581ae94307430b
aroybase <aroybase@gmail.col>
2014-08-14 01:22:54 +0200

    - Finalement non :) (les templates doivent être publics pour être accessibles…)

--- public/js/application/directives/HeaderDirective.js
+++ public/js/application/directives/HeaderDirective.js
@@ -1,7 +1,7 @@
 app.directive('ngHeader', function () {
     return{
         restrict: 'E',
-        templateUrl: '../views/_directives/Header.html',
+        templateUrl: '_directives/Header.html',
         scope: {
             user: '=',
             notifications: '='
